dd94d4c6533e15f3d1b1ebcf5451c2783bae04ec
migasQ <migasQ@users.noreply.github.com>
2020-02-02 16:46:53 +0100

    improvements from https://github.com/wazum/openconnect-proxy

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # How to use
-```sh
-$ docker-compose up --abort-on-container-exit
+`docker-compose up -d` with
+
 ```
+  V`export PN_NAME=""
+  export ANYCONNECT_SERVER=""
+  export ANYCONNECT_USER=""
+  export ANYCONNECT_OPTIONS="--servercert ..."
+  export ANYCONNECT_PING=""
+  #export ANYCONNECT_PASSWORD=""
+``
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-( echo yes; echo $ANYCONNECT_PASSWORD ) | openconnect $ANYCONNECT_SERVER --user=$ANYCONNECT_USER --timestamp
+echo $ANYCONNECT_PASSWORD | openconnect -u $ANYCONNECT_USER $ANYCONNECT_OPTIONS  --timestamp -b --passwd-on-stdin $ANYCONNECT_SERVER
+echo "Openconnect established. Starting to ping in 30s."
+sleep 30
+while [ 1 ] ; do ping -c 2 $ANYCONNECT_PING ; sleep 60 ; done
